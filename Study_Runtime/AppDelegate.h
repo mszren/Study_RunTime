01e40a2563f3733c1b2f72913b581f61e75f68eb
Binbin Shen <shen.binbin@rytong.com>
2017-06-08 13:17:08 +0800

    Initial Commit

---
+++ Study_Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study_Runtime
+//
+//  Created by rytong on 2017/6/8.
+//  Copyright © 2017年 rytong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Study_RuntimeTests/Study_RuntimeTests.m
@@ -0,0 +1,39 @@
+//
+//  Study_RuntimeTests.m
+//  Study_RuntimeTests
+//
+//  Created by rytong on 2017/6/8.
+//  Copyright © 2017年 rytong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Study_RuntimeTests : XCTestCase
+
+@end
+
+@implementation Study_RuntimeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
